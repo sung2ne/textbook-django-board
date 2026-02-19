2957ed6612e5251d56aa0e3b28e6d680b1e82bc2
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-19 13:46:07 +0900

    part09/chapter-01: 01장. 슈퍼 유저 기능 이용하기

--- mysite/posts/admin.py
+++ mysite/posts/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Post
+from comments.models import Comment
 
-admin.site.register(Post)
+# 게시글을 조회할 때 댓글도 함께 조회되도록 처리
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+    
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_at', 'updated_by', 'updated_at')
+    search_fields = ('title', 'created_by__username')
+    list_filter = ('created_at',)
+    inlines = [CommentsInline]
+
+admin.site.register(Post, PostsAdmin)
