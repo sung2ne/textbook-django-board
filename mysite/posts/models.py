29138ec57758d36a499991c355c58530be036904
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-19 13:46:06 +0900

    part07/chapter-01: 01장. 게시글 모델 수정하기

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # 게시글 모델
 class Post(models.Model):
     title = models.CharField(verbose_name="제목", max_length=100)
     content = models.TextField(verbose_name="내용")
-    password = models.CharField(verbose_name="비밀번호", max_length=100)
-    username = models.CharField(verbose_name="글쓴이", max_length=10)
     filename = models.CharField(verbose_name="파일명", max_length=100, null=True, blank=True)
     original_filename = models.CharField(verbose_name="원본파일명", max_length=100, null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="작성자", null=True, blank=True, related_name="post_created_by")
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="수정자", null=True, blank=True, related_name="post_updated_by")
     created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
     
