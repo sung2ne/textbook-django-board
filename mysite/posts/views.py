b1d307e2d4f0a9a140522ee8357cd46ab42e885a
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-19 13:46:06 +0900

    part07/chapter-07: 07장. 게시글 첨부파일 수정하기

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -17,40 +17,40 @@ from .forms import PostCreateForm, PostUpdateForm
 @login_required(login_url='auth:login')
 def create_post(request):
     form = PostCreateForm()
-
+    
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
-
+        
         if form.is_valid():
             post = form.save(commit=False)
             post.created_by = request.user
             post.updated_by = request.user
             post.save()
-
+            
             # 파일 업로드
             if request.FILES.get('uploadFile'):
                 filename = uuid.uuid4().hex
                 file = request.FILES.get('uploadFile')
-
+                
                 # 파일 저장 경로
                 file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(filename))
                 if not os.path.exists(os.path.dirname(file_path)):
                     os.makedirs(os.path.dirname(file_path))
-
+                
                 # 파일 저장
                 with open(file_path, 'wb') as f:
                     for chunk in file.chunks():
                         f.write(chunk)
-
+                
                 post.filename = filename
                 post.original_filename = file.name
                 post.save()
-
+            
             messages.success(request, '게시글이 등록되었습니다.')
             return redirect("posts:read", post_id=post.id)
         else:
             messages.error(request, '게시글 등록에 실패했습니다.')
-
+    
     return render(request, 'posts/create.html', {'form': form})
 
 # 게시글 보기
@@ -62,31 +62,31 @@ def get_post(request, post_id):
 # 게시글 수정
 @login_required(login_url='auth:login')
 def update_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-
+    post = get_object_or_404(Post, id=post_id) 
+       
     if post.created_by != request.user:
         messages.error(request, '게시글 수정 권한이 없습니다.')
         return redirect('posts:read', post_id=post.id)
-
+    
     form = PostUpdateForm(instance=post)
-
+    
     if request.method == 'POST':
         form = PostUpdateForm(request.POST, instance=post)
-
+        
         if form.is_valid():
             post.title = form.cleaned_data['title']
             post.content = form.cleaned_data['content']
             post.updated_by = request.user
             post.save()
-
+                
             # 파일 삭제
             if request.POST.get('deleteFile'):
                 if post.filename:
                     # 파일 삭제
-                    file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))
+                    file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))    
                     if os.path.exists(file_path):
                         os.remove(file_path)
-
+                    
                     post.filename = None
                     post.original_filename = None
                     post.save()
@@ -95,27 +95,27 @@ def update_post(request, post_id):
             if request.FILES.get('uploadFile'):
                 if post.filename:
                     # 파일 삭제
-                    file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))
+                    file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))    
                     if os.path.exists(file_path):
                         os.remove(file_path)
-
+                    
                 filename = uuid.uuid4().hex
                 file = request.FILES.get('uploadFile')
-
+                
                 # 파일 저장 경로
                 file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(filename))
                 if not os.path.exists(os.path.dirname(file_path)):
                     os.makedirs(os.path.dirname(file_path))
-
+                
                 # 파일 저장
                 with open(file_path, 'wb') as f:
                     for chunk in file.chunks():
                         f.write(chunk)
-
+                        
                 post.filename = filename
                 post.original_filename = file.name
                 post.save()
-
+                
             messages.success(request, '게시글이 수정되었습니다.')
             return redirect('posts:read', post_id=post.id)
         else:
@@ -127,36 +127,36 @@ def update_post(request, post_id):
 @login_required(login_url='auth:login')
 def delete_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-
+    
     if post.created_by != request.user:
         messages.error(request, '게시글 삭제 권한이 없습니다.')
         return redirect('posts:read', post_id=post.id)
-
+    
     if request.method == 'POST':
         # 파일 삭제
         if post.filename:
-            file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))
+            file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))    
             if os.path.exists(file_path):
                 os.remove(file_path)
-
+        
         post.delete()
         messages.success(request, '게시글이 삭제되었습니다.')
         return redirect('posts:list')
 
 # 게시글 목록
 @login_required(login_url='auth:login')
-def get_posts(request):
-    page = request.GET.get('page', '1')
+def get_posts(request):    
+    page = request.GET.get('page', '1') 
     searchType = request.GET.get('searchType')
     searchKeyword = request.GET.get('searchKeyword')
     posts = Post.objects.all().order_by('-created_at')
-
+    
     # 검색 조건 처리
     if searchType not in [None, ''] and searchKeyword not in [None, '']:
         if searchType == 'all':
             posts = posts.filter(
-                Q(title__contains=searchKeyword) |
-                Q(content__contains=searchKeyword) |
+                Q(title__contains=searchKeyword) | 
+                Q(content__contains=searchKeyword) | 
                 Q(created_by__first_name__contains=searchKeyword)
             )
         elif searchType == 'title':
@@ -171,34 +171,35 @@ def get_posts(request):
             posts = posts.filter(
                 Q(created_by__first_name__contains=searchKeyword)
             )
-
+    
     # 페이지네이션
     paginator = Paginator(posts, 10)
     page_obj = paginator.get_page(page)
-
+    
     # 현재 페이지의 첫 번째 게시글 번호 계산
     start_index = paginator.count - (paginator.per_page * (page_obj.number - 1))
-
+    
     # 순번 계산하여 게시글 리스트에 추가
     for index, _ in enumerate(page_obj, start=0):
         page_obj[index].index_number = start_index - index
-
+    
     return render(request, 'posts/list.html', {
         'posts': page_obj,
         'searchType': searchType,
         'searchKeyword': searchKeyword
     })
-
+    
 # 첨부 파일 다운로드
+@login_required(login_url='auth:login')
 def download_file(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     file_path = os.path.join(settings.MEDIA_ROOT, 'posts', str(post.id), str(post.filename))
-
+    
     if os.path.exists(file_path):
         with open(file_path, 'rb') as file:
             response = HttpResponse(file.read(), content_type='application/octet-stream')
             encoded_filename = quote(post.original_filename)
             response['Content-Disposition'] = f'attachment; filename*=UTF-8\'\'{encoded_filename}'
             return response
-
+    
     return HttpResponse(status=404)
