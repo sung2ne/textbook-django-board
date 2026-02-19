cae7147cf80bd632bfb8f10f6d13fd2fa4b9c970
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-20 00:53:32 +0900

    fix: UI 감사 버그 수정 (정적파일, 템플릿, 루트URL, 모바일 반응형)

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/posts/', permanent=False)),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
 ]
