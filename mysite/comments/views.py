0b68cbfb4dba8553fcab079892c3c968a4c96f1e
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-19 13:46:06 +0900

    part08/chapter-03: 03장. 댓글 등록 만들기

--- mysite/comments/views.py
+++ mysite/comments/views.py
@@ -1,8 +1,56 @@
 from django.http import JsonResponse
+from django.utils import timezone
+
+from posts.models import Post
+from .models import Comment
+
+# 댓글 목록
+def get_comments(post_id):
+    comments = Comment.objects.filter(post=post_id).order_by('-created_at')
+    comments_list = []
+    for comment in comments:
+        comments_list.append({
+            'id': comment.id,
+            'content': comment.content,
+            'created_by': comment.created_by.first_name,
+            'updated_by': comment.updated_by.first_name,
+            'created_at': comment.created_at.astimezone(timezone.get_current_timezone()).strftime('%Y-%m-%d %H:%M'),
+            'updated_at': comment.updated_at.astimezone(timezone.get_current_timezone()).strftime('%Y-%m-%d %H:%M'),
+        })
+    return comments_list
 
 # 댓글 등록
 def create_comment(request):
-    return JsonResponse({'message': '댓글 등록'})
+    if not request.user.is_authenticated:
+        return JsonResponse({'result': 'error', 'message': '로그인 후 이용해주세요.'})
+    
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        if not post_id:
+            return JsonResponse({'result': 'error', 'message': '게시글을 선택해주세요.'})
+        
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return JsonResponse({'result': 'error', 'message': '게시글을 찾을 수 없습니다.'})
+        
+        content = request.POST.get('content')          
+        if not content:
+            return JsonResponse({'result': 'error', 'message': '댓글 내용을 입력해주세요.'})
+        
+        # 댓글 등록
+        comment = Comment(
+            post=post,
+            content=content,
+            created_by=request.user,
+            updated_by=request.user
+        )
+        comment.save()
+        
+        # 댓글 목록
+        comments = get_comments(post_id)
+
+        return JsonResponse({'result': 'success', 'comments': comments})
 
 # 댓글 수정
 def update_comment(request, comment_id):
